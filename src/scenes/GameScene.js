918e33adfa6f4c9ca79b59994d832c2b8ec7586a
Jerry Noftz <noftzjs@mail.uc.edu>
2020-12-04 23:16:51 -0500

    removed sprite animation for finish up player movement

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -34,10 +34,9 @@ export default class GameScene extends Phaser.Scene
 	create()
 	{
 		this.add.sprite(320, 200, 'forest');
-		const platforms = this.createPlatforms()
 		this.player = this.createPlayer()
 		this.python = this.createPython()
-		this.physics.add.collider(this.player, platforms)
+		// this.physics.add.collider(this.player)
 
 		this.cursors = this.input.keyboard.createCursorKeys()
 	
@@ -45,29 +44,29 @@ export default class GameScene extends Phaser.Scene
 
 	update()
 	{
-		// if (this.cursors.left.isDown)
-		// {
-		// 	this.player.setVelocityX(-160)
+		if (this.cursors.left.isDown)
+		{
+			this.player.setVelocityX(-160)
 
-		// 	this.player.anims.play('left', true)
-		// }
-		// else if (this.cursors.right.isDown)
-		// {
-		// 	this.player.setVelocityX(160)
+			this.player.anims.play('left', true)
+		}
+		else if (this.cursors.right.isDown)
+		{
+			this.player.setVelocityX(160)
 
-		// 	this.player.anims.play('right', true)
-		// }
-		// else
-		// {
-		// 	this.player.setVelocityX(0)
+			this.player.anims.play('right', true)
+		}
+		else
+		{
+			this.player.setVelocityX(0)
 
-		// 	this.player.anims.play('turn')
-		// }
+			this.player.anims.play('turn')
+		}
 
-		// if (this.cursors.up.isDown && this.player.body.touching.down)
-		// {
-		// 	this.player.setVelocityY(-330)
-		// }
+		if (this.cursors.up.isDown && this.player.body.touching.down)
+		{
+			this.player.setVelocityY(-330)
+		}
 	}
 
 	createPlatforms()
@@ -88,20 +87,20 @@ export default class GameScene extends Phaser.Scene
 
 		this.anims.create({
 			key: 'left',
-			frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 3 }),
+			frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 0 }),
 			frameRate: 10,
 			repeat: -1
 		})
 		
 		this.anims.create({
 			key: 'turn',
-			frames: [ { key: PLAYER_KEY, frame: 4 } ],
+			frames: [ { key: PLAYER_KEY, frame: 0 } ],
 			frameRate: 20
 		})
 		
 		this.anims.create({
 			key: 'right',
-			frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 5, end: 8 }),
+			frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 0 }),
 			frameRate: 10,
 			repeat: -1
 		})
